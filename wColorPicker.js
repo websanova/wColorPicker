381eec69887674571ee385ad8642a7d4cf46c070
websanova <rob@websanova.com>
2013-01-19 19:12:42 -0500

    Updating to version 1.3.4

--- wColorPicker.js
+++ wColorPicker.js
@@ -8,7 +8,7 @@
  * @license         This wChar jQuery plug-in is dual licensed under the MIT and GPL licenses.
  * @link            http://www.websanova.com
  * @github			http://github.com/websanova/wColorPicker
- * @version         Version 1.3.3
+ * @version         Version 1.3.4
  *
  ******************************************/
 (function($)
